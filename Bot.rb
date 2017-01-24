a10d5e692d7a695cc5016105615521baf89af861
Karl <Calafska@gmail.com>
2017-01-24 17:49:28 +0100

    fixed stuff
    Also I deleted some commented lines because im adding them later

--- Bot.rb
+++ Bot.rb
@@ -1,16 +1,15 @@
-::RBNACL_LIBSODIUM_GEM_LIB_PATH = "PASTE DLL PATH HERE"
 require 'discordrb'
 
-bot = Discordrb::Commands::CommandBot.new token: 'TOKEN HERE', client_id: 'CLIENT ID HERE', prefix: 'cxd-'
+bot = Discordrb::Commands::CommandBot.new token: 'TOKEN HERE', client_id: CLIENT ID HERE, prefix: 'cxd-'
 
 
-bot.command (:ping, help_available: false) do |event|
+bot.command(:ping, help_available: false) do |event|
   event << 'pong!'
 end
 
-bot.command (:exit, help_available: false) do |event|
+bot.command(:exit, help_available: false) do |event|
   break unless event.user.id == YOUR ID HERE
-  
+
   bot.send_message(event.channel.id, 'Goodbye!')
   exit
 end
@@ -18,20 +17,12 @@ end
 #commands
 #Respond and pm commands
 
-bot.command (:source) do |event|
+bot.command(:source) do |event|
   event.respond ('https://github.com/Calafska/CXD-Bot')
 end
 
-bot.command (:xtheman166) do |event|
+bot.command(:xtheman166) do |event|
   event.respond ('https://github.com/Xtheman166')
 end
-
-#Music/Voice commands
-
-#Line of codes here ( music/voice )
-
-#Other commands here
-
-#line of code here ( other commands )
 
 bot.run
