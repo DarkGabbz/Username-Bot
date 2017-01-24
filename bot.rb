99b0fd038e707847ab004fb49ca3b591f8f85b4f
Xavier V <xtheman166@gmail.com>
2017-01-24 14:59:34 -0500

    Revert #1f6bd1b

--- bot.rb
+++ bot.rb
@@ -1,6 +1,6 @@
 require 'discordrb'
 
-bot = Discordrb::Commands::CommandBot.new token: 'TOKEN HERE', client_id: CLIENT ID HERE, prefix: '69'
+bot = Discordrb::Commands::CommandBot.new token: 'TOKEN HERE', client_id: CLIENT ID HERE, prefix: '&&'
 
 #Commands
 
