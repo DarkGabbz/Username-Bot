bf32b1be442949b1265355bd78407888877942ee
Karl <Calafska@gmail.com>
2017-01-24 16:39:40 +0100

    Add files via upload
    Uploaded main ruby code.

---
+++ Bot.rb
@@ -0,0 +1,21 @@
+require 'discordrb'
+
+bot = Discordrb::Bot.new token: 'TOKEN HERE', client_id: 'CLIENT ID HERE'
+
+
+bot.command (:ping) do |event|
+  event << 'pong!'
+end
+
+bot.command (:exit, help_available: false) do |event|
+  bot.send_message(event.channel.id, 'Goodbye!')
+  exit
+end
+
+#commands
+
+bot.command (:source) do |event|
+  event.respond ('https://github.com/Calafska/CX-Bot')
+end
+
+bot.run 
