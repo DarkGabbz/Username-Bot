375b2352a4aaf37d7b7ade35f3aae2491f4f3e30
Kingy <thekingy35@gmail.com>
2017-01-24 17:22:05 +0000

    Cleaned up code with comments
    -TK

--- Bot.rb
+++ Bot.rb
@@ -2,10 +2,15 @@ require 'discordrb'
 
 bot = Discordrb::Commands::CommandBot.new token: 'TOKEN HERE', client_id: CLIENT ID HERE, prefix: 'cxd-'
 
+#Commands
+
+#To make sure the bot is online,
 
 bot.command(:ping) do |event|
   event << 'pong!'
 end
+
+#Changes what the bot is playing (<3 Kingy)
 
 bot.command(:game, description: 'Changes the game played by the bot. BOT OWNER ONLY') do |event, *game|
     unless event.user.id == YOUR_ID
@@ -16,19 +21,23 @@ bot.command(:game, description: 'Changes the game played by the bot. BOT OWNER O
             event.respond("Game set to `#{game.join(' ')}`")
 end
 
+#Kill the bot, BOT OWNER ONLY
+
 bot.command(:exit, help_available: false) do |event|
   break unless event.user.id == YOUR ID HERE
-
+  #Doesn't accept other users
+  
   bot.send_message(event.channel.id, 'Goodbye!')
   exit
 end
 
-#commands
 #Respond and pm commands
 
 bot.command(:source) do |event|
   event.respond ('https://github.com/Calafska/CXD-Bot')
 end
+
+#Creator commands
 
 bot.command(:xtheman166) do |event|
   event.respond ('https://github.com/Xtheman166')
