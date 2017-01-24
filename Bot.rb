0182ad0ec8e9a72e3b2d9e58356fb5e13c0238b1
Kingy <thekingy35@gmail.com>
2017-01-24 17:03:23 +0000

    Update Bot.rb

--- Bot.rb
+++ Bot.rb
@@ -3,6 +3,17 @@ require 'discordrb'
 bot = Discordrb::Commands::CommandBot.new token: 'TOKEN HERE', client_id: CLIENT ID HERE, prefix: 'cxd-'
 
 
+# <3 - Kingy
+
+bot.command(:game, description: 'DESCRIPTION HERE') do |event, *game|
+    unless event.user.id == YOUR_ID
+           event.respond("TEXT FOR NOT MATCHING ID HERE")
+                break
+              end
+            event.bot.game = game.join(' ')
+            event.respond("MESSAGE WHEN IT WORKS, GAME TITLE = `#{game.join(' ')}`")
+end
+
 bot.command(:ping, help_available: false) do |event|
   event << 'pong!'
 end
@@ -23,35 +34,6 @@ end
 
 bot.command(:xtheman166) do |event|
   event.respond ('https://github.com/Xtheman166')
-end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# <3 - Kingy
-
-bot.command(:game, description: 'DESCRIPTION HERE') do |event, *game|
-    unless event.user.id == YOUR_ID
-           event.respond("TEXT FOR NOT MATCHING ID HERE")
-                break
-              end
-            event.bot.game = game.join(' ')
-            event.respond("MESSAGE WHEN IT WORKS, GAME TITLE = `#{game.join(' ')}`")
 end
 
 bot.run
